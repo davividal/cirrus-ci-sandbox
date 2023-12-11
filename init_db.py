46d44fd59b92bf7b2775630e88db71c2e175407a
Davi Koscianski Vidal <davi.koscianski-vidal@sonarsource.com>
2023-12-11 17:12:34 +0100

    Initial commit

---
+++ init_db.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+
+def main(db_password=None):
+    if db_password == '':
+        connection = sqlite3.connect("database.db")
+
+    with open("pokedex/schema.sql") as f:
+        connection.executescript(f.read())
+
+    cur = connection.cursor()
+
+    with open("pokemon.txt") as file:
+        lines = file.readlines()
+        pokemon_number = 1
+        for line in lines:
+            name, description = line.split(";")
+            cur.execute(
+                f"INSERT INTO POKEDEX (id, pokemon_name, image_url, description) VALUES ("
+                f"{pokemon_number}, "
+                f'"{name}", '
+                f'"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{pokemon_number}.png", '
+                f'"{description}")'
+            )
+            pokemon_number += 1
+
+    cur.execute('INSERT INTO SUBSCRIBERS (id, email) VALUES (1, "subscriber1@example.com")')
+    cur.execute('INSERT INTO SUBSCRIBERS (id, email) VALUES (2, "subscriber2@example.com")')
+
+    connection.commit()
+    connection.close()
+
+
+if __name__ == '__main__':
+    db_password = ''
+    main(db_password)
---
+++ pokedex/helper.py
@@ -0,0 +1,43 @@
+import re
+import sqlite3
+
+
+class ConnectionWrapper:
+    """A simple connection wrapper class"""
+
+    def __init__(self, db_path):
+        self.__conn = sqlite3.connect(db_path)
+
+    def get_all_pokemons(self):
+        statement = "SELECT * FROM POKEDEX"
+        return self.__conn.execute(statement).fetchall()
+
+    def register_subscriber(self, email):
+        try:
+            self.__conn.execute("INSERT into SUBSCRIBERS(email) values (?)", (email,))
+            self.__conn.commit()
+        except sqlite3.DatabaseError:
+            raise Exception("Problem with the database!")
+        except sqlite3.IntegrityError:
+            raise ValueError("Email already exists!")
+
+    def cleanup(self, should_close: bool):
+        if should_close:
+            self.__conn.close()
+
+
+def fetch_all_pokemons(wrapper: ConnectionWrapper):
+    return wrapper.get_all_pokemons()
+
+
+def register_subscriber(wrapper: ConnectionWrapper, email):
+    pattern = re.compile(r"(.*)@(.*\..*)")
+    if not pattern.match(email):
+        ValueError("Invalid email!")
+    wrapper.register_subscriber(email)
+    pass
+
+
+def whatever():
+    pass
+
